99b23af1009d29cb8648e5243fa5a4648ef94bc7
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-08-23 19:19:26 -0500

    vídeo03 - Atributos útiles de phpunit

--- testing02_examples/exs03_php_test_calculator/tests/unit/StackTest.php
+++ testing02_examples/exs03_php_test_calculator/tests/unit/StackTest.php
@@ -1,17 +1,30 @@
 <?php declare(strict_types=1);
 
-    use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
 
-    final class StackTest extends TestCase{
+final class StackTest extends TestCase {
 
-        public function test_sumar(): void {
-            
-            $num1 = 1;
-            $num2 = 2;
+    #[DataProvider('dataProvider')]
+    #[TestDox('Al Sumar el $num1 con $num2 el resultado es $expected')]
+    #[Test]
+    public function sumar(int $expected, int $num1, int $num2): void {
 
-            // Comprobaciones de afirmaciones
-            $this->assertSame(3, $num1 + $num2);
+        // Comprobaciones de afirmaciones
+        $this->assertSame($expected, $num1 + $num2);
+    }
 
-        }
+    public static function dataProvider(){
+
+        return [
+            'data set 1' => [0, 0, 0],
+            'data set 2' => [1, 0, 1],
+            'data set 3' => [1, 1, 0],
+            'data set 4' => [3, 1, 1]
+        ];
 
     }
+
+}
